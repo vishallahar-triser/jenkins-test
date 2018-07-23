35724f3b6b9034616e44c0abdfdd1ce16a6e7414
Vishal <vishal.lahar@triser.com>
2018-07-23 16:08:35 -0400

    groovy test

---
+++ jobs/testscript.groovy
@@ -0,0 +1,13 @@
+job('jobtest') {
+    logRotator(-1, 10)
+    scm {
+        git('git://github.com/vishallahar-triser/jenkins-test.git')
+    }
+    triggers {
+        scm('* * * * *')
+    }
+    steps {
+        batchFile('javac test.java')
+      	batchFile('java test')
+    }
+}
