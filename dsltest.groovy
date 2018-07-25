7ad5769e11f4f4bbf6cedc8ae6b665f988b27bd9
Vishal <vishal.lahar@triser.com>
2018-07-25 10:08:43 -0400

    test

--- dsltest.groovy
+++ dsltest.groovy
@@ -3,11 +3,26 @@ def x = 20
 1.upto(5, {
     println (x+ it) })
 
-String greet(String otherPerson) {
+class Person {
+    /** the name of the person */
+    String name
+
+    /**
+     * Creates a greeting method for a certain person.
+     *
+     * @param otherPerson the person to greet
+     * @return a greeting message
+     */
+    String greet(String otherPerson) {
        "Hello ${otherPerson}"
     }
+}
+def p = new Person()
+def p.name = 'Vishal'
+println p.greet("Vishal")
 
-println greet("Vishal")
+
+
 def env = System.getenv()
 env.each{
 println it
